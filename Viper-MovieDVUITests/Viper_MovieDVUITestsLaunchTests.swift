4b1aab65d4a999ff630ed6710b57bd77946d62ec
Arif Rahman Sidik <sidik@sgo.co.id>
2023-03-22 15:28:02 +0700

    Initial Commit

---
+++ Viper-MovieDVUITests/Viper_MovieDVUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Viper_MovieDVUITestsLaunchTests.swift
+//  Viper-MovieDVUITests
+//
+//  Created by Arif Rahman Sidik on 22/03/23.
+//
+
+import XCTest
+
+final class Viper_MovieDVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
